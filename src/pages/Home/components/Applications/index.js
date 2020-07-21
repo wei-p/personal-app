25717c155cf636b80c80a2d75235886c497ed533
Wei Pan <wp2251@columbia.edu>
2020-07-20 23:19:41 -0400

    Applications in progress

--- src/pages/Home/components/Applications/index.js
+++ src/pages/Home/components/Applications/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { List, Tag } from 'antd'
+import styles from './index.module.less'
 
-const Applications = () => {
+const Applications = ({ list }) => {
   return (
-    <div>
-      Applications
-    </div>
+    <List
+      className={styles.applicationList}
+      rowKey='id'
+      grid={{ gutter: 24, xxl: 3, xl: 2, lg: 2, md: 2, sm: 2, xs: 1}}
+      dataSource={list}
+      renderItem={(item) => (
+        <List.Item
+          key={item.id}
+        >
+          absd
+        </List.Item>
+      )}
+    >
+
+    </List>
   )
 }
 
